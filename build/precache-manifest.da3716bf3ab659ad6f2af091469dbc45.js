4426de1a6737a812296d36fe8bc8a473af0ed557
Steven Noble <snoble@rawnet.com>
2019-05-02 16:23:22 +0100

    Added production build

---
+++ build/precache-manifest.da3716bf3ab659ad6f2af091469dbc45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5a655bd2f215e204b227",
+    "url": "/static/js/main.a7845011.chunk.js"
+  },
+  {
+    "revision": "93cd55f45a7a88bb648d",
+    "url": "/static/js/2.e27cf4de.chunk.js"
+  },
+  {
+    "revision": "5a655bd2f215e204b227",
+    "url": "/static/css/main.7a27b2b8.chunk.css"
+  },
+  {
+    "revision": "e7e4a2b54349dddd77f386438ef7ddcc",
+    "url": "/index.html"
+  }
+];
